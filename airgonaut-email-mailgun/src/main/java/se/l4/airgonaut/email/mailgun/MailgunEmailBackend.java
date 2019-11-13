5f62639b5213f874a0d4ec2d068014231a809e83
Andreas Holstenson <a@holstenson.se>
2019-11-13 20:47:11 +0100

    feat(email-mailgun): Initial code for Mailgun backend

---
+++ airgonaut-email-mailgun/src/main/java/se/l4/airgonaut/email/mailgun/MailgunEmailBackend.java
@@ -0,0 +1,82 @@
+package se.l4.airgonaut.email.mailgun;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import kong.unirest.HttpResponse;
+import kong.unirest.MultipartBody;
+import kong.unirest.Unirest;
+import se.l4.airgonaut.channels.EmailChannel;
+import se.l4.airgonaut.email.EmailBackend;
+import se.l4.airgonaut.email.EmailChannelEncoding;
+import se.l4.airgonaut.email.RenderedEmail;
+
+/**
+ * {@link EmailBackend} for sending e-mails using the Mailgun Rest API.
+ */
+public class MailgunEmailBackend
+	implements EmailBackend
+{
+	/**
+	 * URL used for Mailgun in most of the world.
+	 */
+	public static final String BASE_URL = "https://api.mailgun.net/v3";
+
+	/**
+	 * URL used for Mailgun within the EU.
+	 */
+	public static final String BASE_URL_EU = "https://api.eu.mailgun.net/v3";
+
+	private final String endpoint;
+	private final String apiKey;
+
+	public MailgunEmailBackend(
+		String baseUrl,
+		String domain,
+		String apiKey
+	)
+	{
+		this.endpoint = baseUrl + '/' + domain + "/messages";
+		this.apiKey = apiKey;
+	}
+
+	@Override
+	public void send(RenderedEmail email)
+		throws IOException
+	{
+		MultipartBody request = Unirest.post(endpoint)
+			.basicAuth("api", apiKey)
+			.field("to", toAddresses(email.getTo()))
+			.field("cc", toAddresses(email.getCC()));
+
+		if(email.getPlainText().isPresent())
+		{
+			request = request.field("text", email.getPlainText().get());
+		}
+
+		if(email.getHTML().isPresent())
+		{
+			request = request.field("html", email.getHTML().get());
+		}
+
+		// TODO: From?
+
+		HttpResponse<?> reply = request.asJson();
+		if(! reply.isSuccess())
+		{
+			throw new IOException("Unable to send e-mail via Mailgun");
+		}
+	}
+
+	private List<String> toAddresses(List<EmailChannel> emails)
+	{
+		List<String> result = new ArrayList<>(emails.size());
+		for(EmailChannel email : emails)
+		{
+			result.add(EmailChannelEncoding.encode(email));
+		}
+
+		return result;
+	}
+}
